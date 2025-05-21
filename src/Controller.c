752efa0cf9aa16277406fe2c10c5765402bcf395
GRiveraDev7 <gonzalorivera1908@gmail.com>
2025-05-21 18:14:55 +0800

    Final code

--- src/Controller.c
+++ src/Controller.c
@@ -10,6 +10,9 @@
 #define ADC_MAX_VALUE     253
 #define SEND_INTERVAL_MS  100
 
+uint8_t mode = 0;
+int current_ms = 0;
+
 int main(void)
 {
   // Initialization
@@ -20,12 +23,25 @@ int main(void)
   lcd_init();
   _delay_ms(20);
 
+  // Interrupt setup 
+  DDRD &= ~(1 << PD1);  // INT1 (PD1)
+  DDRE &= ~(1 << PE4);  // INT2 (PE4)
+
+  // INT1 -> Rising edge
+  EICRA |= (1 << ISC11) | (1 << ISC10); // ISC11:10 = 11 = RISING EDGE
+  // INT2 -> Falling edge
+  EICRA |= (1 << ISC21);
+  EICRA &= ~(1 << ISC20);               // ISC21:20 = 10 = FALLING EDGE
+
+  EIMSK |= (1 << INT1) | (1 << INT2);
+
+  sei();
+
   uint8_t vertical1 = 0;
   uint8_t vertical2 = 0;
   uint8_t horizontal1 = 0;
   uint8_t horizontal2 = 0;
-  uint8_t receivedDataRange[3]; // Received data array
-  int current_ms = 0;
+  uint8_t receivedDataRange[4]; // Received data array
   int last_send_ms = 0;
   char received_string[100] = {};
 
@@ -59,7 +75,7 @@ int main(void)
       }
 
       // Function takes number of bytes followed by data bytes
-      serial2_write_bytes(4, vertical1, horizontal1, vertical2, horizontal2);
+      serial2_write_bytes(4, vertical1, horizontal1, vertical2, mode);
       last_send_ms = current_ms;
     }
 
@@ -67,18 +83,45 @@ int main(void)
     // autonomous/manual, light level/frequency, low battery voltage alert
     if (serial2_available())
     {
-      serial2_get_data(receivedDataRange, 3); // Fill array with 3 bytes
+      serial2_get_data(receivedDataRange, 4); // Fill array with 4 bytes
       lcd_clrscr();
 
       lcd_home();  // Same as lcd_goto(0)
-      sprintf(received_string, "L:%ucm R:%ucm", receivedDataRange[0], receivedDataRange[2]);
+      sprintf(received_string, "L:%ucm R:%ucm", receivedDataRange[0], receivedDataRange[1]);
       lcd_puts(received_string);  // First line
 
       lcd_goto(0x40);  // Cursor to second line
-      sprintf(received_string, "F:%ucm", receivedDataRange[1]);
+      sprintf(received_string, "F:%ucm L: %u.%uHz", receivedDataRange[2], receivedDataRange[3]/10, receivedDataRange[3]%10);
       lcd_puts(received_string);
     }
   }
 
   return 1;
 }
+
+// Interrupt for button 1, Auto mode
+ISR(INT1_vect) {
+  int last_int_ms = 0;
+  if ((last_int_ms - current_ms) > SEND_INTERVAL_MS){
+    if(mode == 1){
+      mode = 0;
+    }else{
+      mode = 1;
+    }
+    last_int_ms = current_ms;
+  }
+}
+
+// Interrupt for button 2, Light mode
+ISR(INT2_vect) {
+  int last_int_ms = 0;
+  if ((last_int_ms - current_ms) > SEND_INTERVAL_MS){
+    if(mode == 2){
+      mode = 0;
+    }else{
+      mode = 2;
+    }
+    last_int_ms = current_ms;
+  }
+}
+
--- src/Robot.c
+++ src/Robot.c
@@ -7,13 +7,15 @@
 #include "Robot.h"
 
 // Constants
-#define MAX_SENSOR_SIDE      50
-#define MAX_SENSOR_FRONT     100
-#define SEND_INTERVAL_MS     100
-#define SERVO_OFFSET         952
-#define SERVO_SCALE          4
-#define PWM_SCALE            8000 / 126
-#define NO_LIGHT_VALUE       220
+#define MAX_SENSOR_SIDE_L   50
+#define MAX_SENSOR_SIDE_R   49
+#define MAX_SENSOR_FRONT    100
+#define SEND_INTERVAL_MS    100
+#define MAX_WAIT_LIGHT_TIME 2000
+#define SERVO_OFFSET        952
+#define SERVO_SCALE         4
+#define PWM_SCALE           8000 / 126
+#define NO_LIGHT_VALUE      180
 
 // PI Control Constants
 #define KP                  20.0f
@@ -22,13 +24,16 @@
 #define KI_LIGHT            0.01f
 #define TOO_CLOSE_FORWARD   15
 #define TOO_CLOSE_SIDE      10
+#define FOLLOW_LIGHT_MODE   2
+#define FOLLOW_PATH_MODE    1
+#define MANUAL_MODE         0
 
 // Function Prototypes
 void auto_drive(uint16_t sensor_forward, uint16_t sensor_left, uint16_t sensor_right);
 void motor_drive(uint16_t speed, uint16_t direction);
 void motor_init();
 void servo(uint16_t control);
-void follow_light_drive(uint16_t right_light_sensor, uint16_t left_light_sensor);
+void follow_light_drive(uint16_t right_light_sensor, uint16_t left_light_sensor, uint16_t sensor_forward);
 
 // Globals
 int16_t error = 0;
@@ -36,6 +41,13 @@ int16_t integral = 0;
 int16_t control = 0;
 int16_t lm = 0;
 int16_t rm = 0;
+int16_t mode = MANUAL_MODE;
+uint16_t compare_value = 0;
+int counter = 0;
+int seconds = 0;
+int minutes = 0;
+int current_ms = 0;
+int found_light_ms = 0;
 
 int main(void)
 {
@@ -49,7 +61,6 @@ int main(void)
 
     uint8_t receivedData[4];
     char serial_string[120] = {0};
-    int current_ms = 0;
     int last_send_ms = 0;
     int battery = 0;
 
@@ -61,44 +72,88 @@ int main(void)
     // Light Sensors
     uint8_t left_light = 0;
     uint8_t right_light = 0;
+    uint8_t light_average = 0;
+
+    // Frequency
+    uint16_t time_low = 0;
+    uint16_t frequency = 0;
+    bool time_low_flag = true;
+    bool time_high_flag = true;
 
     while (1)
     {
         current_ms = milliseconds_now();
+        // Light Sensors
+        right_light = (adc_read(4) >> 2);
+        left_light = (adc_read(5) >> 2);
+        light_average = (right_light + left_light)/2;
+        
 
+        // Light Frequency Section
+        if(time_low_flag && (light_average < 160)){
+            time_low = counter;
+            time_low_flag = false;
+            time_high_flag = true;
+        }
+        if(time_high_flag && (light_average > 180)){
+            frequency = 1 * 10000 / (counter - time_low);
+            counter = 0;
+            time_low = 0;
+            time_low_flag = true;
+            time_high_flag = false;
+            // sprintf(serial_string, "\n Freq: %u.%u Hz", frequency/10, frequency%10);
+            // serial0_print_string(serial_string);
+        }
+        
         // Sending Section
         if ((current_ms - last_send_ms) >= SEND_INTERVAL_MS)
-        {
+        {    
+            sprintf(serial_string, "\n Left Light: %u, Right Light: %u", 255-left_light, 255-right_light);
+            serial0_print_string(serial_string);
+
             // Distance Sensors
             left_sensor  = 280 / ((adc_read(0) >> 2) / 2);
             front_sensor = 280 / ((adc_read(1) >> 2) / 2);
             right_sensor = 280 / ((adc_read(2) >> 2) / 2);
 
-            // Light Sensors
-            right_light = (adc_read(4) >> 2);
-            left_light = (adc_read(5) >> 2);
-
-            if (left_sensor > MAX_SENSOR_SIDE) left_sensor = MAX_SENSOR_SIDE;
-            if (right_sensor > MAX_SENSOR_SIDE) right_sensor = MAX_SENSOR_SIDE;
+            if (left_sensor > MAX_SENSOR_SIDE_L) left_sensor = MAX_SENSOR_SIDE_L;
+            if (right_sensor > MAX_SENSOR_SIDE_R) right_sensor = MAX_SENSOR_SIDE_R;
             if (front_sensor > MAX_SENSOR_FRONT) front_sensor = MAX_SENSOR_FRONT;
 
-            battery = adc_read(3 >> 2);
+            battery = (adc_read(3) >> 2);
 
-            serial2_write_bytes(4, left_sensor, front_sensor, right_sensor);
+            serial2_write_bytes(4, left_sensor, right_sensor, front_sensor, frequency);
             last_send_ms = current_ms;
 
-            follow_light_drive(right_light, left_light);
+            // follow_light_drive(right_light, left_light);
+        }
+
+        if(battery>180){
+            PORTA |= (1 << PA3);
+        }else{
+            PORTA &= ~(1 << PA3);
         }
 
         // Receiving Section
         if (serial2_available())
         {
             serial2_get_data(receivedData, 4);
+            mode = receivedData[3];
         }
+        
 
-        // servo(receivedData[2]);
-        // motor_drive(receivedData[0], receivedData[1]);
-        // auto_drive(front_sensor, left_sensor, right_sensor);
+        // if(mode == FOLLOW_LIGHT_MODE){
+        //     follow_light_drive(right_light, left_light, front_sensor);
+        //     // servo(125);
+        // }else if(mode == FOLLOW_PATH_MODE){
+        //     auto_drive(front_sensor, left_sensor, right_sensor);
+        //     // servo(125);
+        // }else if(mode == MANUAL_MODE){
+        //     motor_drive(receivedData[0], receivedData[1]);
+        //     servo(receivedData[2]);
+        // }
+
+        auto_drive(front_sensor, left_sensor, right_sensor);
         
     }
 
@@ -141,13 +196,32 @@ void motor_drive(uint16_t horizontal_direction, uint16_t speed)
 }
 
 void motor_init()
-{
+{   
+    
+
     TCCR1A = (1 << COM1A1) | (1 << COM1B1) | (1 << COM1C1);
     TCCR1B = (1 << WGM13) | (1 << CS11);
     ICR1 = 20000;
 
     DDRB |= (1 << PB0) | (1 << PB1) | (1 << PB5) | (1 << PB6) | (1 << PB7);
-    DDRA |= (1 << PA0) | (1 << PA1);
+    DDRA |= (1 << PA0) | (1 << PA1) | (1 << PA3);
+
+    // Setup for Timer4
+    TCCR4B |= (1 << WGM42) | (1 << CS41) | (1 << CS40); // Prescaler 64, CTC Mode (WGM4[3:0] = 0100)
+    TCCR4A = 0;
+
+    OCR4A = 249; // 16MHz / (64 * (1 + 249)) = 1kHz = 1ms
+
+    TIMSK4 |= (1 << OCIE4A); // Enable interrupt on compare match A
+}
+
+// ======================
+// Timer 3 Interrupt Service Routine
+// ======================
+
+ISR(TIMER4_COMPA_vect) {
+    // This runs every 1ms
+    counter++;
 }
 
 // ======================
@@ -160,9 +234,6 @@ void auto_drive(uint16_t sensor_forward, uint16_t sensor_left, uint16_t sensor_r
     if (sensor_forward < TOO_CLOSE_FORWARD)
     {
         // Turn to one side
-        sprintf(serial_string, "\n I am here instead");
-        serial0_print_string(serial_string);
-
         if (sensor_left > sensor_right) {
             motor_drive(126, 20);   // Turn Right
         } else {
@@ -172,9 +243,6 @@ void auto_drive(uint16_t sensor_forward, uint16_t sensor_left, uint16_t sensor_r
     else if (sensor_left < TOO_CLOSE_SIDE || sensor_right < TOO_CLOSE_SIDE)
     {
         // PI Control
-        sprintf(serial_string, "\n I am here broooo");
-        serial0_print_string(serial_string);
-
         error = (int16_t)sensor_left - (int16_t)sensor_right;
         integral += error;
 
@@ -190,24 +258,25 @@ void auto_drive(uint16_t sensor_forward, uint16_t sensor_left, uint16_t sensor_r
     else
     {   
       // Drive straight
-        sprintf(serial_string, "\n I am here");
-        serial0_print_string(serial_string);
-
         motor_drive(0, 127);
     }
 }
-void follow_light_drive(uint16_t right_light_sensor, uint16_t left_light_sensor)
+void follow_light_drive(uint16_t right_light_sensor, uint16_t left_light_sensor, uint16_t sensor_forward)
 {
-    char serial_string[120] = {0};
+    uint16_t velocity = 50;
 
-    if (right_light_sensor > NO_LIGHT_VALUE && left_light_sensor > NO_LIGHT_VALUE)
+    if(sensor_forward < TOO_CLOSE_FORWARD){
+        velocity = 127;
+    }else{
+        velocity = 50;
+    }
+
+    if (((current_ms - found_light_ms) > MAX_WAIT_LIGHT_TIME) && (right_light_sensor > NO_LIGHT_VALUE && left_light_sensor > NO_LIGHT_VALUE))
     {
         // Turn to one side
-        sprintf(serial_string, "\n I am here instead");
-        serial0_print_string(serial_string);
         motor_drive(126, 20);   // Turn Right
     }
-    else{
+    else if (right_light_sensor < NO_LIGHT_VALUE && left_light_sensor < NO_LIGHT_VALUE){
         // PI Control for light
         error = (int16_t)right_light_sensor - (int16_t)left_light_sensor - 4; // 4 is the offset for the light sensor
         integral += error;
@@ -218,11 +287,10 @@ void follow_light_drive(uint16_t right_light_sensor, uint16_t left_light_sensor)
         if (control_f < -126) control_f = -126;
 
         control = (int16_t)control_f;
-        if(control>0) sprintf(serial_string, "\n Turning Left, control: %i, sensor_left: %u, sensor_right: %u", control, left_light_sensor, right_light_sensor);
-        else sprintf(serial_string, "\n Turning Right, control: %i", control);
-        serial0_print_string(serial_string);
+        
+        motor_drive(velocity, 127 - control);
 
-        motor_drive(50, 127 - control);
+        found_light_ms = current_ms;
     }
 }
 
